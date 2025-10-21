8e41505a09eee821df89feff5227e2b7f7572126
Lokasorus <knightyt07@gmail.com>
2025-10-21 19:00:40 +0530

    Fix: added toast import properly

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
